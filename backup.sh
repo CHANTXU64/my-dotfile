582ad5dfdefe11a56aa333250b0b74c08545160f
CHANTXU64 <chantxu@outlook.com>
2022-12-24 12:41:27 +0800

    shell

---
+++ backup.sh
@@ -0,0 +1,14 @@
+cp ~/.gitconfig ./gitconfig
+cp ~/.npm.certs.pem ./npm.certs.pem
+cp ~/.npmrc ./npmrc
+cp ~/.zprofile ./zprofile
+cp ~/.zshrc ./zshrc
+cp ~/.config/alacritty.yml ./config/alacritty.yml
+cp ~/.config/com.googlecode.iterm2.plist ./config/com.googlecode.iterm2.plist
+cp -r ~/.config/htop ./config/
+cp -r ~/.config/karabiner ./config/
+cp -r ~/.config/nvim ./config/
+cp -r ~/.config/ranger ./config/
+cp -r ~/.config/sketchybar ./config/
+cp -r ~/.config/skhd ./config/
+cp -r ~/.config/yabai ./config/
---
+++ update_from_git.sh
@@ -0,0 +1,6 @@
+cp ./gitconfig ~/.gitconfig
+cp ./npm.certs.pem ~/.npm.certs.pem
+cp ./npmrc ~/.npmrc
+cp ./zprofile ~/.zprofile
+cp ./zshrc ~/.zshrc
+cp -r ./config ~/.config
